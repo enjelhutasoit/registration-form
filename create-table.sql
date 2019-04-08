840d995f01dc1b602cc04e4be713539f04f53684
Enjel Hutasoit <28510687+enjelhutasoit@users.noreply.github.com>
2019-04-09 03:15:55 +0700

    Create create-table.sql

---
+++ create-table.sql
@@ -0,0 +1,8 @@
+create table [dbo].[Registration](
+    id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    job VARCHAR(30),
+    age INT,
+    date DATE
+);
